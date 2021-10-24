e202e546943e6f0ca7f0f71202afa3400c8c9993
Virginijus03 <77634732+Virginijus03@users.noreply.github.com>
2021-10-24 16:30:16 +0300

    buttonai, sarasas ir t.t.

--- src/app/components/krepselis/krepselis.component.ts
+++ src/app/components/krepselis/krepselis.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Preke } from 'src/app/models/preke';
 
 @Component({
   selector: 'app-krepselis',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./krepselis.component.scss']
 })
 export class KrepselisComponent implements OnInit {
+  @Input()
+  public prekes?: Preke[];
 
+  public rodomosPrekes: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  public krepselioKiekis(): number {
+    let kiekis = 0;
+    this.prekes?.forEach((preke) => {
+      kiekis += preke.kiekis;
+    })
+    return kiekis;
+  };
+
+  public krepselioTurinys(): Preke[] {
+    const krepselioPrekes: Preke[] = [];
+    this.prekes?.forEach((preke) => {
+      if (preke.kiekis > 0) {
+        krepselioPrekes.push(preke);
+      }
+    });
+    return krepselioPrekes;
+  };
+
+  public pakeistiRodyma() {
+    this.rodomosPrekes = !this.rodomosPrekes
+  };
 
 }
